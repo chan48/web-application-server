18e5e0320e8eefee18d7b1aa02f65da16e73249b
김정환 <6pack@woowahan.com>
2017-04-06 22:00:00 +0900

    과제1 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,20 +22,64 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "utf8"));
+            String line = br.readLine();
+
+            if (line == null) {
+                return;
+            }
+
+            log.debug("{}", line);
+
+            String method = "";
+            String path = "";
+
+            while (!"".equals(line)) {
+                String [] tokens = line.split(" ");
+
+                if (Objects.equals(tokens[0], "GET")) {
+                    method = tokens[0];
+                    path = tokens[1];
+                    break;
+                }
+
+                line = br.readLine();
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+
+            byte[] body;
+            String type = "plain";
+
+            if (path.equals("/")) {
+                body = "Hello World".getBytes();
+            } else {
+                body = Files.readAllBytes(new File("./webapp" + path).toPath());
+
+                String[] exts = path.split("\\.");
+                String ext = exts[exts.length -1];
+                log.debug("exts: {}", ext);
+
+                type = ext;
+                if (ext == "js") {
+                    type = "javascript";
+                }
+            }
+
+            response200Header(dos, body.length, type);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String type) {
+        String contentType = "Content-Type: text/" + type + ";charset=utf-8\r\n";
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(contentType);
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
